59f26967d0294b80f2fdaf22228a91d1075327b2
FauricioCespedes <75187297+FauricioCRC@users.noreply.github.com>
2025-03-09 16:18:22 -0600

    fix(multiidioma): El multiidioma ya no redirecciona al login

--- src/main/java/com/G4/AsociacionSolidarista/controller/IndexController.java
+++ src/main/java/com/G4/AsociacionSolidarista/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-     @GetMapping("/")
+    @GetMapping("/")
     public String home() {
         return "index"; // Redirige a index.html en templates
     }
---
+++ src/main/resources/static/js/multiidioma.js
@@ -0,0 +1,12 @@
+/*const es = document.getElementById('es')
+const en = document.getElementById('en')
+
+const baseUrl = window.location.origin + window.location.pathname
+
+es.addEventListener('click', e => {
+    window.location.href = baseUrl + "?lang=es"
+})
+
+en.addEventListener('click', e => {
+    window.location.href = baseUrl + "?lang=en"
+})*/

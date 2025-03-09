3625e709a08a237ff9fe99b340ee17ac137869e3
FauricioCespedes <75187297+FauricioCRC@users.noreply.github.com>
2025-03-09 16:47:29 -0600

    feat(/login): Se agrega el endpoint /login

--- src/main/java/com/G4/AsociacionSolidarista/controller/IndexController.java
+++ src/main/java/com/G4/AsociacionSolidarista/controller/IndexController.java
@@ -1,14 +1,25 @@
-
 package com.G4.AsociacionSolidarista.controller;
 
+import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
+
     @GetMapping("/")
     public String home() {
         return "index"; // Redirige a index.html en templates
     }
-    
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password) {
+
+        System.out.println(email);
+        System.out.println(password);
+
+        return "redirect:/ahorro/listado";
+    }
 }

569f364104eb7071d1e8066e41665503d5f1c8ef
vjulio <vjuliorc@gmail.com>
2025-03-28 00:25:04 -0600

    SEMANA 11.2 BONUS REQUEST MATCHER

---
+++ src/main/java/com/G4/AsociacionSolidarista/domain/RequestMatcher.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.G4.AsociacionSolidarista.domain;
+
+/**
+ *
+ * @author Julio
+ */
+public class RequestMatcher {
+    
+}
